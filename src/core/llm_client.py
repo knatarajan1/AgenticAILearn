28323eb7aacca76392ba0a1cb004999709f2a472
knatarajan1 <mailtoknk1394@gmail.com>
2025-08-31 19:25:52 +0530

    katest commit

---
+++ src/core/llm_client.py
@@ -0,0 +1,7 @@
+import os
+from typing import List,Dict
+import httpx as tex
+from dotenv import load_dotenv
+from __future__ import annotations
+
+
---
+++ src/core/utils.py
@@ -0,0 +1,10 @@
+import os
+from typing import List,Dict
+import httpx as tex
+from dotenv import load_dotenv
+from __future__ import annotations
+from pathlib import Path
+import json
+
+
+
